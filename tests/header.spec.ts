89c2a6aca6bfa7e5014ec98f2dab52cff73aabab
k-majick <studio@cyberleaf.pl>
2020-11-24 14:59:27 +0100

    jest config update

--- tests/header.spec.ts
+++ tests/header.spec.ts
@@ -4,6 +4,6 @@ import { mount } from '@vue/test-utils';
 describe('Header', () => {
   it('is a Vue component', () => {
     const wrapper = mount(Header);
-    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.vm).toBeTruthy();
   });
 });
